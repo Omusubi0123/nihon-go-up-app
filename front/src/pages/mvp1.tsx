0d297384404441c9c02706a21dc7341d752e4cc5
andotnct <mityugame@gmail.com>
2024-09-07 23:28:23 +0900

    update: モードによって出すモーダルを変更

--- front/src/pages/mvp1.tsx
+++ front/src/pages/mvp1.tsx
@@ -19,8 +19,8 @@ import { useState } from 'react';
 
 export default function mvp1() {
   const { isOpen, onOpen, onClose } = useDisclosure(); // モーダルの制御
-  const [text, setText] = useState("日々の生活は忙しさに満ちていますが、時折立ち止まって自分自身と向き合うことが大切です。忙しい日常の中で見過ごしがちな小さな喜びや、他者とのふれあいを大切にしましょう。時間に追われる中でも、心の余裕を持つことで、自分の目標や夢に向かって確実に前進することができます。自己成長や人間関係の発展には、継続的な努力と、時には一息つくことが必要です。");
-  const [convertedText, setConvertedText] = useState("日々の生活は忙しさに満ちていますが、時折立ち止まって自分自身と向き合うことが大切です。忙しい日常の中で見過ごしがちな小さな喜びや、他者とのふれあいを大切にしましょう。時間に追われる中でも、心の余裕を持つことで、自分の目標や夢に向かって確実に前進することができます。自己成長や人間関係の発展には、継続的な努力と、時には一息つくことが必要です。");
+  const [text, setText] = useState("");
+  const [convertedText, setConvertedText] = useState("");
   const [inputText, setInputText] = useState(""); // モーダル内で入力されたテキスト
   const [imageSrc, setImageSrc] = useState(""); // 選択された画像のURL
   const [imageExtension, setImageExtension] = useState(""); // 選択された画像の拡張子
--- front/src/pages/mvp2.tsx
+++ front/src/pages/mvp2.tsx
@@ -1,8 +1,24 @@
-import { Button, VStack, HStack, Box, Image as ChakraImage, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, Input, Center } from '@chakra-ui/react';
+import {
+  Button,
+  VStack,
+  HStack,
+  Box,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Textarea,
+  Center,
+  Image,
+} from '@chakra-ui/react';
 import { useState, useRef } from 'react';
 
 export default function Mvp2() {
-  const { isOpen, onOpen, onClose } = useDisclosure(); // モーダルの制御
+  const { isOpen: isOpenModal1, onOpen: onOpenModal1, onClose: onCloseModal1 } = useDisclosure(); // モーダルの制御
+  const { isOpen: isOpenModal2, onOpen: onOpenModal2, onClose: onCloseModal2 } = useDisclosure(); // モーダルの制御
   const [inputText, setInputText] = useState(""); // モーダル内で入力されたテキスト
   const [imageSrc, setImageSrc] = useState<string | null>(null); // 画像のURLを格納
   const [componentImageSrc, setComponentImageSrc] = useState<string | null>(null); // 画像のURLを格納
@@ -105,19 +121,22 @@ export default function Mvp2() {
           style={{ display: 'none' }} // inputを非表示にする
           onChange={handleFileChange}
         />
-        <Button width="100%" colorScheme="blue" size="lg" onClick={onOpen}>
-          画像をアップロード
+        <Button width="100%" colorScheme="blue" size="lg" onClick={onOpenModal1}>
+          画像説明モード1
+        </Button>
+        <Button width="100%" colorScheme="blue" size="lg" onClick={onOpenModal2}>
+          画像説明モード2
         </Button>
       </VStack>
       <VStack flex="1" p={4} bg="gray.200" align="start" spacing={4}>
         {imageSrc && (
           <Box flex="1" p={4}>
-            <ChakraImage src={imageSrc} alt="Uploaded Image" maxH="300px" objectFit="contain" />
+            <Image src={imageSrc} alt="Uploaded Image" maxH="300px" objectFit="contain" />
           </Box>
         )}
       </VStack>
-      {/* モーダル */}
-      <Modal isOpen={isOpen} onClose={onClose}>
+      {/* モーダル1 */}
+      <Modal isOpen={isOpenModal1} onClose={onCloseModal1}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>画像を選択してください</ModalHeader>
@@ -129,23 +148,50 @@ export default function Mvp2() {
             </Center>
             {componentImageSrc && (
               <Box flex="1" p={4}>
-                <ChakraImage src={componentImageSrc} alt="Uploaded Image" maxH="300px" objectFit="contain" />
+                <Image src={componentImageSrc} alt="Uploaded Image" maxH="300px" objectFit="contain" />
               </Box>
             )}
           </ModalBody>
           <ModalHeader>文章を入力してください</ModalHeader>
           <ModalBody>
-            <Input
+            <Textarea
               placeholder="文章をここに入力"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
+              size="lg"
+              resize="vertical"  // 必要に応じてリサイズを許可
+              rows={10}  // 行数を指定
             />
           </ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={handleModalSubmit}>
               送信
             </Button>
-            <Button variant="ghost" onClick={onClose}>キャンセル</Button>
+            <Button variant="ghost" onClick={onCloseModal1}>キャンセル</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {/* モーダル2 */}
+      <Modal isOpen={isOpenModal2} onClose={onCloseModal2}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>モード2</ModalHeader>
+          <ModalHeader>文章を入力してください</ModalHeader>
+          <ModalBody>
+            <Textarea
+              placeholder="文章をここに入力"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+              size="lg"
+              resize="vertical"  // 必要に応じてリサイズを許可
+              rows={10}  // 行数を指定
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleModalSubmit}>
+              送信
+            </Button>
+            <Button variant="ghost" onClick={onCloseModal2}>キャンセル</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
