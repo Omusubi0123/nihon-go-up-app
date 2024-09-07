74a070e1fff6a6aede18172fb74806df91678e7c
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-09-07 23:55:24 +0900

    feat: 画像をテキストに変換するDB作成

---
+++ back/src/image_to_text.py
@@ -0,0 +1,97 @@
+import base64
+import json
+import sys
+from typing import Generator
+
+import boto3
+
+from src.prompts.image_prompt import IMAGE_TO_TEXT_FOR_RAG_PROMPT
+from src.settings import Settings
+
+
+def local_image_to_data(image_path: str) -> bytes:
+    with open(image_path, "rb") as f:
+        b64_image_data = base64.b64encode(f.read()).decode("utf-8")
+    return b64_image_data
+
+
+def create_body(
+    b64_image_data: bytes,
+) -> str:
+    messages = [
+        {
+            "role": "user",
+            "content": [{"type": "text", "text": IMAGE_TO_TEXT_FOR_RAG_PROMPT}],
+        },
+    ]
+    messages[0]["content"].append(
+        {
+            "type": "image",
+            "source": {
+                "type": "base64",
+                "media_type": f"image/jpeg",
+                "data": b64_image_data,
+            },
+        }
+    )
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 1000,
+            "temperature": 0.5,
+            "messages": messages,
+        }
+    )
+    return body
+
+
+def llm_process_image(
+    b64_image_data: bytes,
+) -> Generator[str, None, None]:
+    settings = Settings()
+    session = boto3.Session(profile_name=settings.aws_username)
+    client = session.client(service_name="bedrock-runtime", region_name="us-east-1")
+    response = client.invoke_model_with_response_stream(
+        modelId="anthropic.claude-3-5-sonnet-20240620-v1:0",
+        body=create_body(b64_image_data),
+    )
+    stream = response.get("body")
+    combined_text = ""
+    if stream:
+        for event in stream:
+            chunk = event.get("chunk")
+            if chunk:
+                data = json.loads(chunk.get("bytes").decode())
+                content = data.get("content_block", {}).get("text") or data.get(
+                    "delta", {}
+                ).get("text")
+                if content:
+                    combined_text += content
+                    print(content, end="")
+                    sys.stdout.flush()
+    return combined_text
+
+
+def main():
+    import json
+    from glob import glob
+
+    image_paths = glob("data/dog_use/*.jpeg")
+
+    image_and_text = []
+    for image_path in image_paths:
+        print("\nprocessing image:", image_path)
+        b64_image_data = local_image_to_data(image_path)
+        llm_process_image(b64_image_data)
+        image_and_text.append(
+            {
+                "description": llm_process_image(b64_image_data),
+                "id": image_path.split("/")[-1].split(".")[0],
+            }
+        )
+    with open("data/dog_use.json", "w") as f:
+        json.dump(image_and_text, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
