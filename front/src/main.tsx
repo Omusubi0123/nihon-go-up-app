21f681f883af30aac7a1d70867d5369ce1266a14
Omusubi0123 <Vermouth77rseidl@gmail.com>
2025-01-18 04:29:25 +0900

    fix6

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Mvp1 from './pages/mvp1'
 import Mvp2 from './pages/mvp2'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -10,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ChakraProvider>
       <Router basename="/">
         <Routes>
+          <Route path="/" element={<Navigate to="/input" replace />} />
           <Route path="/input" element={<Mvp1 />} />
           <Route path="/output" element={<Mvp2 />} />
         </Routes>
